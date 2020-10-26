89c4eb029cae30f0731be4014d66b83ce7259bb3
n51363148 <gokaygulsoy@std.iyte.edu.tr>
2020-10-26 11:20:08 +0000

    Seasons matching program

---
+++ lab2/example5.py
@@ -0,0 +1,10 @@
+a = input("Please insert a month and day: ")
+
+if a == "March 20" :
+  print("Spring")
+elif a =="June 21":
+  print("Summer")
+elif a == "September 22" :
+  print("Fall")
+elif a == "December 21":
+  print("Winter") 
